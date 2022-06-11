d00ec05538a71c546553825c2bafb18981a9bb43
Phong Dương <phongdxn@fiisoft.com>
2022-06-11 09:57:46 +0700

    Update config authen Firebase

---
+++ app/src/main/java/com/example/mycommicreader/view/Login.java
@@ -0,0 +1,75 @@
+package com.example.mycommicreader.view;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mycommicreader.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+public class Login extends Fragment {
+    private FirebaseAuth mAuth;
+//    private FirebaseDatabase database;
+//    private DatabaseReference myRef;
+//    private FirebaseFirestore firestore;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View e = inflater.inflate(R.layout.fragment_login, container, false);
+        Button btn_login = e.findViewById(R.id.login);
+        EditText email = e.findViewById(R.id.email);
+        EditText pass = e.findViewById(R.id.password);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String em = email.getText().toString();
+                String pa = pass.getText().toString();
+                login(em,pa);
+//                Navigation.findNavController(view).navigate(R.id.list);
+            }
+        });
+        return  e;
+    }
+    private void login(String email, String password){
+        mAuth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("DEBUG", "signInWithEmail:success");
+                            Toast.makeText(getContext().getApplicationContext(), "Authentication successful.",Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("DEBUG", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(getContext().getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+}
